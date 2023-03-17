4411836a3e930d523504b0d55369d98c1439c8a1
httapia <leotapia24@gmail.com>
2023-03-16 22:09:16 -0300

    Second commit

---
+++ src/components/DatePickerRange/DatePickerRange.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import dayjs, { Dayjs } from 'dayjs';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateRange } from '@mui/x-date-pickers-pro';
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+
+export default function DateRangePickerValue() {
+  const [value, setValue] = React.useState<DateRange<Dayjs>>([
+    dayjs('2022-04-17'),
+    dayjs('2022-04-21'),
+  ]);
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DateRangePicker', 'DateRangePicker']}>
+        <DemoItem label="DATEPICKER RANGE TEST" component="DateRangePicker">
+          <DateRangePicker
+            defaultValue={[dayjs('2022-04-17'), dayjs('2022-04-21')]}
+          />
+        </DemoItem>
+
+      </DemoContainer>
+    </LocalizationProvider>
+  );
+}
